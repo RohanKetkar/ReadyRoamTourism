c94a07760841a9e9fd572d368d1f6127161e26c4
Rohan Ketkar <yourgitemail@example.com>
2024-12-07 20:40:51 +0530

    [rohan] added base entity

--- Backend/ReadyRome/src/main/java/com/tour/entities/BaseEntity.java
+++ Backend/ReadyRome/src/main/java/com/tour/entities/BaseEntity.java
@@ -15,6 +15,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+//Base
 @MappedSuperclass // class level annotation to specify following is a base class from which other
 // entities will inherit , no separate table !
 @Getter
